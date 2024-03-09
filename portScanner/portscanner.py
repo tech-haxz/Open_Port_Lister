38254fabef83c173920ba8c70df1b0903297f44e
V1N4Y <111497577+tech-haxz@users.noreply.github.com>
2024-03-09 11:12:36 +0530

    Added file

---
+++ portScanner/portscanner.py
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+import socket
+import sys
+from datetime import datetime
+
+if len(sys.argv)==2:
+	target = socket.gethostbyname(sys.argv[1])
+		
+else:
+	print("Invalid arguments")
+	print("Syntax: python3 portscanner.py 192.168.202.43")
+	
+#Banner
+
+print("_" *50)
+print(f"Scanning Ip: {target}")
+print(f"Time: {datetime.now()}")
+print("_" *50)
+
+
+try:
+	print("This will take a while....")
+	print("Take a cup of coffee.")
+	for port in range(50, 90):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target, port))
+		
+		if result == 0:
+			print(f"Port {port} is open")
+			
+		s.close()
+		
+except KeyboardInterrupt:
+	print("\nExinting program....")
+	sys.exit()
+
+except socket.gaierror:
+	print("Hostname could not resolve")
+	sys.exit()
+	
+except socket.error:
+	print("Could not connet to server")
+	sys.exit()
